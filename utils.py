9063b1a9a069b5b33a4961ee4189b02834cd9681
Darryl Hamilton <darryl@addict.net.nz>
2018-12-28 15:26:21 +1300

    Black formatting

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ p = engine()
 
 
 def format_float(num):
-    return format(num, '.15g')
+    return format(num, ".15g")
 
 
 def sort_counter(results, ordinal=True, convert_int=True):
@@ -22,54 +22,54 @@ def sort_counter(results, ordinal=True, convert_int=True):
 
 def list_or_none(values):
     if len(values) > 0:
-        return ', '.join(values)
+        return ", ".join(values)
     else:
         return "None"
 
 
 def add_row(data, pos, teams_disabled):
     if teams_disabled:
-        data.append([
-            p.ordinal(pos['position']),
-            pos['name'],
-            format_float(pos['points'])
-        ])
+        data.append(
+            [p.ordinal(pos["position"]), pos["name"], format_float(pos["points"])]
+        )
     else:
-        data.append([
-            p.ordinal(pos['position']),
-            pos['name'],
-            pos['team']['name'],
-            format_float(pos['points'])
-        ])
+        data.append(
+            [
+                p.ordinal(pos["position"]),
+                pos["name"],
+                pos["team"]["name"],
+                format_float(pos["points"]),
+            ]
+        )
 
     return data
 
 
 def format_delta(delta):
-    string_delta = ''
+    string_delta = ""
     if delta.months != 0:
-        string_delta = f'{abs(delta.months)} months'
+        string_delta = f"{abs(delta.months)} months"
         if delta.days != 0:
-            string_delta = f'{string_delta}, {abs(delta.days)} days'
+            string_delta = f"{string_delta}, {abs(delta.days)} days"
     elif delta.days != 0:
-        string_delta = f'{abs(delta.days)} days'
+        string_delta = f"{abs(delta.days)} days"
 
     if delta.months < 0 or delta.days < 0:
-        string_delta = f'{string_delta} ago'
+        string_delta = f"{string_delta} ago"
 
     return string_delta
 
 
 def get_current_season(division, bot):
-    if division.lower() not in bot.config['division_season']:
-        r = bot.session.get(f'{bot.base_url}/api/info/{division.lower()}')
+    if division.lower() not in bot.config["division_season"]:
+        r = bot.session.get(f"{bot.base_url}/api/info/{division.lower()}")
         info = json.loads(r.content)
-        if 'season' not in info:
+        if "season" not in info:
             return False
 
-        bot.config['division_season'][division.lower()] = str(info['season']['id'])
-        if str(info['season']['id']) not in bot.config['season_info']:
-            bot.config['season_info'][str(info['season']['id'])] = info['season']
+        bot.config["division_season"][division.lower()] = str(info["season"]["id"])
+        if str(info["season"]["id"]) not in bot.config["season_info"]:
+            bot.config["season_info"][str(info["season"]["id"])] = info["season"]
 
         bot.save_config()
 
