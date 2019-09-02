2c3f429778bdc97d66d207e0b38821737acb3bf1
Darryl Hamilton <darryl@addict.net.nz>
2019-09-02 15:58:11 +1200

    Add F1 Fantasy cog to base bot

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 from discord.ext import commands
 from cogs.rlmbot import RLMBot
+from cogs.f1fantasy import F1Fantasy
 
 bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or("!"), description="RLM Bot"
+    command_prefix=commands.when_mentioned_or("+"), description="RLM Bot"
 )
-
 
 @bot.event
 async def on_ready():
@@ -12,4 +12,5 @@ async def on_ready():
 
 
 bot.add_cog(RLMBot(bot))
+bot.add_cog(F1Fantasy(bot))
 bot.run("TOKEN", reconnect=True)
